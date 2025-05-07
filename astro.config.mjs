cb417bf2ed589e416ca7e03dd399e9c1a1a6903d
Gabo2447 <gabrieldeolival2@gmail.com>
2025-05-07 00:42:36 -0400

    feat(home): :sparkles: add news section
    Added a new "News" section to the homepage to highlight recent updates and features.  This section includes a visually distinct style with gradient backgrounds and badges for each update type (New, Improvement, Beta). It improves user awareness of the latest changes and enhances the overall user experience

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula', // Puedes elegir el tema que prefieras
+      langs: ['astro', 'javascript', 'typescript', 'css', 'html', 'json'], // Asegúrate de incluir 'astro' y otros lenguajes que uses
+    },
+  },
 });
